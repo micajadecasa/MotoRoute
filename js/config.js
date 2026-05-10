fdb431889bc681c2d39cd5a0bd7c2a0824f23de6
micajadecasa <micajadecasa@gmail.com>
2026-05-10 17:54:00 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
     MAPBOX_TOKEN: 'pk.eyJ1IjoibWljYWphZGVjYXNhIiwiYSI6ImNtb3lmbW9ncDA0Z2IydHIyaTNsajc2djUifQ.pROJ-awAsKm7qvwTQuL3OA',
     
     // URL de tu implementación de Google Apps Script
-    API_BASE: 'https://script.google.com/macros/s/AKfycbzXfjgUoLjuvCcTE9udgHDkQBViyJCvkHEGK14t0CyMTTOSKyjrxQ4WntstFVG--pLeAA/exec',
+    API_BASE: 'https://script.google.com/macros/s/AKfycbwygi0ddyv9TUNXydjhGRvbe-xX0M0_lZUtIf0FjgigY57isN5JzfX_uY83yYFzOQr23w/exec',
     
     // Configuración de la App
     APP_NAME: 'MotoRoutes',
